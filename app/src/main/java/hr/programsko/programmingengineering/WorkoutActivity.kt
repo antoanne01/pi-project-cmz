786afdc94b5f83c0c282f998eb3944dc26ece296
antoanne01 <covic_a.20@protonmail.com>
2023-12-19 17:09:11 +0100

    myCommit

---
+++ app/src/main/java/hr/programsko/programmingengineering/WorkoutActivity.kt
@@ -0,0 +1,11 @@
+package hr.programsko.programmingengineering
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workout)
+    }
+}
