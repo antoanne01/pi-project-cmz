c6aa22c775353a83d21de539b00a1b65db9b476b
antoanne01 <covic_a.20@protonmail.com>
2023-12-21 15:30:24 +0100

    onStart() - updated to WorkoutActivity::class.java

--- app/src/main/java/hr/programsko/programmingengineering/WorkoutActivity.kt
+++ app/src/main/java/hr/programsko/programmingengineering/WorkoutActivity.kt
@@ -1,11 +1,31 @@
 package hr.programsko.programmingengineering
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import hr.programsko.programmingengineering.databinding.ActivitySignUpBinding
+import hr.programsko.programmingengineering.databinding.ActivityWorkoutBinding
 
 class WorkoutActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWorkoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_workout)
+        binding = ActivityWorkoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnContinue.setOnClickListener {
+            val weight = binding.txtEditText.text.toString()
+            val height = binding.txtEditTextHeight.text.toString()
+
+            if (weight.isNotEmpty() && height.isNotEmpty()) {
+                val intent = Intent(this, SetWorkoutGoal::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Both weight and height fields must be filled in.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
