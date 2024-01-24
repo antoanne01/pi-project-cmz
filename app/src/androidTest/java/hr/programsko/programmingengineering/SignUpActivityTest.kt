1ee28123f5d58027d8e3aeed3a97df52603f624f
majetik <karlmajetic@gmail.com>
2024-01-24 12:44:19 +0100

    signup activity test - 2 new tests

--- app/src/androidTest/java/hr/programsko/programmingengineering/SignUpActivityTest.kt
+++ app/src/androidTest/java/hr/programsko/programmingengineering/SignUpActivityTest.kt
@@ -1,5 +1,11 @@
 package hr.programsko.programmingengineering
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import org.junit.Assert.*
@@ -56,5 +62,27 @@ class SignUpActivityTest{
             assertFalse(it.isSuccessful)
             assertTrue(it.exception is FirebaseAuthInvalidCredentialsException)
         }
+    }
+
+
+    @Test
+    fun testUIElementsExistence() {
+        val scenario = ActivityScenario.launch(SignUpActivity::class.java)
+
+        onView(withId(R.id.emailEt)).check(matches(isDisplayed()))
+        onView(withId(R.id.passET)).check(matches(isDisplayed()))
+        onView(withId(R.id.confirmPassEt)).check(matches(isDisplayed()))
+        onView(withId(R.id.textView)).check(matches(isDisplayed()))
+        onView(withId(R.id.button)).check(matches(isDisplayed()))
+
+        scenario.close()
+    }
+    @Test
+    fun testButtonClick() {
+        val scenario = ActivityScenario.launch(SignUpActivity::class.java)
+
+        onView(withId(R.id.button)).perform(click())
+
+        scenario.close()
     }
 }
